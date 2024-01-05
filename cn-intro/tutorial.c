df34c1585bbe2b92d1518d76fedc62eb2c762ca5
septract <miked@galois.com>
2024-01-05 16:22:37 +0000

    A file with some simple CN examples

---
+++ cn-intro/tutorial.c
@@ -0,0 +1,198 @@
+// Some simple CN examples 
+// Mike Dodds - Galois Inc - January 2024 
+
+// To get CN to watch this file for modifications: 
+//   $ echo "tutorial.c" | entr -c ../cn/cn tutorial.c
+
+/*
+Question dump: 
+- Is there a way to add inline assertions? 
+- What's going on with the 'take' syntax? Something like the inhale / exhale? in
+  Dafny? 
+*/
+
+// A main() function that does nothing. This can't fault, and therefore requires
+// no CN annotations. 
+
+int main() {
+    return 0;
+}
+
+// A function that adds two numbers together. This can overflow, but we can add
+// a requires-clause that enforces that x and y are both zero. This makes the
+// function trivially non-faulting. 
+
+signed int add_1(signed int x, signed int y) 
+/*@ requires x == 0; y == 0 @*/
+/*@ ensures return == {x}@start + {y}@start @*/
+{ 
+    signed int i; 
+    i = x + y; 
+    return i; 
+}
+
+// This function is also trivially non-faulting if we only require one integer
+// to be zero. 
+
+signed int add_2(signed int x, signed int y) 
+/*@ requires x == 0 @*/
+/*@ ensures return == {x}@start + {y}@start @*/
+{ 
+    signed int i; 
+    i = x + y; 
+    return i; 
+}
+
+// This is the example from the paper. We need to constrain the value of i so
+// that it cannot overflow. 
+
+signed int inc_1(signed int i)
+/*@ requires i < power(2,31) - 1 @*/
+/*@ ensures return == {i}@start + 1 @*/
+{
+    i = i + 1;
+    return i;
+} 
+
+// Verifying the addition function for arbitrary inputs requires that x and y
+// are both non-negative and the total cannot overflow. 
+
+signed int add_3(signed int x, signed int y) 
+/*@ requires 0 <= x; 0 <= y @*/ // <-- Uncertain why this is necessary? 
+/*@ requires x + y < power(2,31) @*/
+/*@ ensures return == {x}@start + {y}@start @*/
+{ 
+    signed int i; 
+    i = x + y; 
+    return i; 
+}
+
+// Modified example from the paper written by OliverB. Note that the syntax in
+// the CN paper is wrong. 
+
+struct s {
+    int x; 
+    int y;
+};
+
+void zero_y_1(struct s *p)
+/*@ requires take OP  = Owned(p) @*/
+/*@ ensures  take OP2 = Owned(p) @*/
+/*@ ensures  OP2.x == OP.x @*/
+/*@ ensures  OP2.y == 0 @*/
+{
+    p->y = 0;
+    // p->x = 7;  // <-- This would fail 
+}
+
+// A function that swaps values. 
+
+void swap_1(int *a, int *b) 
+/*@ requires take Pa = Owned(a) @*/ 
+/*@ requires take Pb = Owned(b) @*/
+/*@ ensures take Qa = Owned(a) @*/
+/*@ ensures take Qb = Owned(b) @*/
+/*@ ensures Qb == Pa @*/
+/*@ ensures Qa == Pb @*/
+{
+    int temp = *a; 
+    *a = *b;
+    // *a = 0;  // <-- This would fail 
+    *b = temp;
+}
+
+
+// A linked list of integers -- struct def and recursive predicate. Taken from:
+// https://github.com/rems-project/cerberus/blob/master/tests/cn/append.c 
+
+struct int_list { 
+  int val;
+  struct int_list* next;
+};
+
+/*@
+datatype seq { 
+  Seq_Nil {},
+  Seq_Cons {integer val, datatype seq next}
+}
+
+predicate (datatype seq) IntList(pointer p) {
+  if (is_null(p)) { 
+    return Seq_Nil{};
+  } else { 
+    take H = Owned<struct int_list>(p);
+    assert (is_null(H.next) || (integer)H.next != 0);
+    take tl = IntList(H.next);
+    return (Seq_Cons { val: H.val, next: tl });
+  }
+}
+@*/
+
+
+// A function on lists that does nothing 
+
+struct int_list* list_do_nothing( struct int_list *xs ) 
+/*@ requires take Xs = IntList(xs) @*/
+/*@ ensures take Ys = IntList(return) @*/
+/*@ ensures Ys == Xs @*/
+{ 
+    struct int_list *ys; 
+    ys = xs; 
+    return ys; 
+}
+
+
+// A predicate that says every list node is 7 
+
+// /*@ 
+// predicate (datatype seq) IntList7(pointer p) {
+//   if (is_null(p)) { 
+//     return Seq_Nil{};
+//   } else { 
+//     take H = Owned<struct int_list>(p);
+//     assert (is_null(H.next) || (integer)H.next != 0);
+//     take tl = IntList7(H.next);
+//     return (Seq_Cons { val: H.val, next: tl });
+//   }
+// }
+// @*/ 
+
+// void list_set_to_7( struct int_list *head) 
+// /*@ requires take Xs = IntList(head) @*/
+// /*@ ensures take Ys = IntList(head) @*/
+// {
+//     struct int_list *curr; 
+//     curr = head; 
+
+//     while (curr != 0) 
+//     /*@ inv take whileInv = IntList(head) @*/
+//     { 
+//         curr->val = 7; 
+//         curr = curr->next; 
+//     }
+//     return;  
+// }
+
+// // A list reverse function - TODO 
+
+// struct int_list* list_reverse( struct int_list *head ) 
+// /*@ requires take Xs = IntList(head) @*/
+// /*@ ensures take Ys = IntList(return) @*/
+// { 
+//     struct int_list *curr; 
+//     curr = head; 
+
+//     struct int_list *prev;
+//     prev = 0; 
+//     struct int_list *next; 
+//     next = 0; 
+
+//     // while (curr != 0) { 
+//     //     next = curr->next; 
+//     //     curr->next = prev; 
+//     //     prev = curr; 
+//     //     curr = next; 
+//     // }
+//     curr = prev;
+//     return curr; 
+// }
