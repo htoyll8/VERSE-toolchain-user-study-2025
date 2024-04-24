a0cf6a40e50e2b6ea99823122e5cf828c700ce07
Sam Cowger <sam@galois.com>
2024-04-24 11:53:42 -0700

    client: create basic client app with VSCode build/run infrastructure

---
+++ cn-lsp/client/src/extension.ts
@@ -0,0 +1,33 @@
+import * as vsc from 'vscode';
+
+import * as ct from 'vscode-languageclient/node';
+
+let client: ct.LanguageClient;
+
+export function activate(context: vsc.ExtensionContext): void {
+    // For the moment, don't refer to a particular server - we should still be
+    // able to do some client-side development without one
+    const serverPath = context.asAbsolutePath("nonexistent");
+    const serverOptions: ct.Executable = {
+        command: serverPath,
+        transport: ct.TransportKind.stdio,
+    };
+
+    const clientOptions: ct.LanguageClientOptions = {
+        documentSelector: [
+            { scheme: 'file', language: 'c' },
+        ]
+    };
+
+    client = new ct.LanguageClient('CN', 'cn', serverOptions, clientOptions);
+    client.start();
+    console.log("started client");
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    } else {
+        return client.stop();
+    }
+}
