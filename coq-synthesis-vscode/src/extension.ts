51fc51d5924d338513199107c510bfde4a8d3c8c
Stuart Pernsteiner <spernsteiner@galois.com>
2024-11-05 10:45:45 -0800

    synthesis: show webview after synthesizing proof

--- coq-synthesis-vscode/src/extension.ts
+++ coq-synthesis-vscode/src/extension.ts
@@ -22,6 +22,243 @@ function runTerminalAsync(opts: vscode.TerminalOptions): Promise<vscode.Terminal
     });
 }
 
+
+// `SingletonWebViewPanel` is based on `CatCodingPanel` from `webview-sample`
+// in https://github.com/microsoft/vscode-extension-samples, used under the
+// following license:
+//
+// MIT License
+//
+// Copyright (c) 2015 - present Microsoft Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// Helper for managing the webview panel.  At most one such panel can exist at
+// a time.
+//
+class SingletonWebViewPanel {
+    /**
+     * Track the currently panel. Only allow a single panel to exist at a time.
+     */
+    public static currentPanel: SingletonWebViewPanel | undefined;
+
+    public static readonly viewType = 'coqSynthesis';
+
+    private readonly _panel: vscode.WebviewPanel;
+    private readonly _extensionUri: vscode.Uri;
+    private _disposables: vscode.Disposable[] = [];
+
+    private static getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
+        return {
+            // Enable javascript in the webview
+            enableScripts: true,
+
+            // And restrict the webview to only loading content from our
+            // extension's `media` directory.
+            localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
+        };
+    }
+
+    public static createOrShow(extensionUri: vscode.Uri): SingletonWebViewPanel {
+        let column = vscode.window.activeTextEditor?.viewColumn;
+        if (column != undefined) {
+            column += 1;
+        } else {
+            column = vscode.ViewColumn.One;
+        }
+        //const column = vscode.ViewColumn.Beside;
+
+        // If we already have a panel, show it.
+        if (SingletonWebViewPanel.currentPanel) {
+            SingletonWebViewPanel.currentPanel._panel.reveal(column);
+            return SingletonWebViewPanel.currentPanel;
+        }
+
+        // Otherwise, create a new panel.
+        const panel = vscode.window.createWebviewPanel(
+            SingletonWebViewPanel.viewType,
+            'Coq Synthesis',
+            column || vscode.ViewColumn.One,
+            SingletonWebViewPanel.getWebviewOptions(extensionUri),
+        );
+
+        SingletonWebViewPanel.currentPanel = new SingletonWebViewPanel(panel, extensionUri);
+
+        return SingletonWebViewPanel.currentPanel;
+    }
+
+    /*
+    public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+        SingletonWebViewPanel.currentPanel = new SingletonWebViewPanel(panel, extensionUri);
+    }
+   */
+
+    private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+        this._panel = panel;
+        this._extensionUri = extensionUri;
+
+        /*
+        // Set the webview's initial html content
+        this._update();
+        */
+
+        // Listen for when the panel is disposed
+        // This happens when the user closes the panel or when the panel is closed programmatically
+        this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+        /*
+        // Update the content based on view changes
+        this._panel.onDidChangeViewState(
+            () => {
+                if (this._panel.visible) {
+                    this._update();
+                }
+            },
+            null,
+            this._disposables
+        );
+        */
+
+        /*
+        // Handle messages from the webview
+        this._panel.webview.onDidReceiveMessage(
+            message => {
+                switch (message.command) {
+                    case 'alert':
+                        vscode.window.showErrorMessage(message.text);
+                        return;
+                }
+            },
+            null,
+            this._disposables
+        );
+        */
+    }
+
+    /*
+    public doRefactor() {
+        // Send a message to the webview webview.
+        // You can send any JSON serializable data.
+        this._panel.webview.postMessage({ command: 'refactor' });
+    }
+    */
+
+    public postMessage(msg: any) {
+        this._panel.webview.postMessage({ command: 'refactor' });
+    }
+
+    public dispose() {
+        SingletonWebViewPanel.currentPanel = undefined;
+
+        // Clean up our resources
+        this._panel.dispose();
+
+        while (this._disposables.length) {
+            const x = this._disposables.pop();
+            if (x) {
+                x.dispose();
+            }
+        }
+    }
+
+    public setHtml(html: string) {
+        this._panel.title = 'Coq Synthesis';
+        this._panel.webview.html = html;
+    }
+
+    /*
+    private _update() {
+        const webview = this._panel.webview;
+
+        // Vary the webview's content based on where it is located in the editor.
+        switch (this._panel.viewColumn) {
+            case vscode.ViewColumn.Two:
+                this._updateForCat(webview, 'Compiling Cat');
+                return;
+
+            case vscode.ViewColumn.Three:
+                this._updateForCat(webview, 'Testing Cat');
+                return;
+
+            case vscode.ViewColumn.One:
+            default:
+                this._updateForCat(webview, 'Coding Cat');
+                return;
+        }
+    }
+    */
+
+    /*
+    private _updateForCat(webview: vscode.Webview, catName: keyof typeof cats) {
+        this._panel.title = catName;
+        this._panel.webview.html = this._getHtmlForWebview(webview, cats[catName]);
+    }
+    */
+
+    /*
+    private _getHtmlForWebview(webview: vscode.Webview, catGifPath: string) {
+        // Local path to main script run in the webview
+        const scriptPathOnDisk = vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js');
+
+        // And the uri we use to load this script in the webview
+        const scriptUri = webview.asWebviewUri(scriptPathOnDisk);
+
+        // Local path to css styles
+        const styleResetPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css');
+        const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css');
+
+        // Uri to load styles into webview
+        const stylesResetUri = webview.asWebviewUri(styleResetPath);
+        const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+
+        // Use a nonce to only allow specific scripts to be run
+        const nonce = getNonce();
+
+        return `<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+
+                <!--
+                    Use a content security policy to only allow loading images from https or from our extension directory,
+                    and only allow scripts that have a specific nonce.
+                -->
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
+
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+                <link href="${stylesResetUri}" rel="stylesheet">
+                <link href="${stylesMainUri}" rel="stylesheet">
+
+                <title>Cat Coding</title>
+            </head>
+            <body>
+                <img src="${catGifPath}" width="300" />
+                <h1 id="lines-of-code-counter">0</h1>
+
+                <script nonce="${nonce}" src="${scriptUri}"></script>
+            </body>
+            </html>`;
+    }
+    */
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -112,6 +349,13 @@ export function activate(context: vscode.ExtensionContext) {
             console.log('new text = ', s);
             editBuilder.replace(span_range, s);
         });
+
+        let panel = SingletonWebViewPanel.createOrShow(context.extensionUri);
+        panel.setHtml(`<!DOCTYPE html>
+            <html><body>
+            <h1>Hello, World!</h1>
+            </body></html>
+        `);
     });
 
     context.subscriptions.push(disposable);
